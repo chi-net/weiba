5248f37b312fecffaea83dc3b5b173ba25a16d83
chihuo2104 <i@chihuo2104.dev>
2024-12-14 18:37:58 +0800

    chore: add mistaken config sample

--- core/getInfo.go
+++ core/getInfo.go
@@ -52,7 +52,7 @@ func InfoHandler(ctx context.Context, b *bot.Bot, update *models.Update, config 
 		if err != nil {
 			log.Fatalf("Error getting host info: %v", err)
 		}
-		
+
 		msg := "weibabot@canary - A bot which can process authentication of private groups and channels\nSystem Info:\n"
 		msg += "Host:" + hostInfo.Platform + "(Kernel:" + hostInfo.OS + " " + hostInfo.KernelVersion + ")\n"
 		msg += "CPU: " + cpuInfo[0].ModelName + "(" + strconv.Itoa(len(cpuInfo)) + ") " + strconv.FormatFloat(cpuInfo[0].Mhz/1000, 'f', 2, 64) + "GHz " + strconv.FormatFloat(cpuPercent[0], 'f', 2, 64) + "% Used\n"
