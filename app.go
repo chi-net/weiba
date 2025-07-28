f00e8f70c7be54cc43b122ebee661184a0ba0d0e
chihuo2104 <i@chihuo2104.dev>
2025-07-29 01:17:53 +0800

    feat: fix a bug

--- app.go
+++ app.go
@@ -143,7 +143,7 @@ func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
 		}
 	}
 
-	if update.Message != nil && (update.Message.Chat.Type == models.ChatTypeGroup || update.Message.Chat.Type == models.ChatTypeSupergroup) {
+	if update.Message.Text != "" && (update.Message.Chat.Type == models.ChatTypeGroup || update.Message.Chat.Type == models.ChatTypeSupergroup) {
 		if config.Features.Tietie {
 			core.HandleTietie(ctx, b, update)
 		}
