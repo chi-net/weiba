bff4b9b98efa051f30b2e81829167384241a93a0
chihuo2104 <i@chihuo2104.dev>
2025-07-29 21:18:32 +0800

    feat: tietie feature done

--- core/handleTietie.go
+++ core/handleTietie.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -19,6 +20,11 @@ func HandleTietie(ctx context.Context, b *bot.Bot, update *models.Update) {
 	// bot: 'user1' 贴了 'user2'!
 	// fmt.Println(message.Text)
 	if message.Text[0] == '/' {
+		pattern := `^[a-zA-Z0-9/\\]+$`
+		matched, _ := regexp.MatchString(pattern, message.Text)
+		if matched && message.Text[1] != '/' {
+			return
+		}
 		i := 0
 		for message.Text[i] == '/' {
 			i += 1
@@ -41,6 +47,25 @@ func HandleTietie(ctx context.Context, b *bot.Bot, update *models.Update) {
 			msg += "自己\\!"
 		}
 		// fmt.Println(msg)
+		b.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID:    chat.ID,
+			Text:      msg,
+			ParseMode: models.ParseModeMarkdown,
+			ReplyParameters: &models.ReplyParameters{
+				MessageID: message.ID,
+				ChatID:    chat.ID,
+			},
+		})
+	} else if message.Text[0] == '\\' {
+		i := 0
+		for message.Text[i] == '\\' {
+			i += 1
+		}
+		receive := strings.SplitN(message.Text[i:], " ", 2)
+		msg := "[" + bot.EscapeMarkdown(sender.FirstName+" "+sender.LastName) + "](tg://user?id=" + strconv.FormatInt(sender.ID, 10) + ") 被 "
+		msg += "[" + bot.EscapeMarkdown(message.ReplyToMessage.From.FirstName+" "+message.ReplyToMessage.From.LastName) + "](tg://user?id=" + strconv.FormatInt(message.ReplyToMessage.From.ID, 10) + ")"
+		msg += receive[0] + "了" + " "
+
 		b.SendMessage(ctx, &bot.SendMessageParams{
 			ChatID:    chat.ID,
 			Text:      msg,
